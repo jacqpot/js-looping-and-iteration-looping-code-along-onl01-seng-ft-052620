abccdc1e4a6d423deab9300179f3b84b6d60410e
Jacques_Potgieter <Potgieter.g.jacques@gmail.com>
2020-08-25 11:20:42 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+
+
+function writeCards(people, event) {
+    let array =[]
+    for (let i = 0; i < people.length; i++) {
+        array.push(`Thank you, ${people[i]}, for the wonderful ${event} gift!`)
+    }
+    return array;
+}
+
+function countDown(number) {
+    while ( number > 0 ) {
+        console.log( number );
+        number -= 1;
+    }
+    console.log(number);
+}
